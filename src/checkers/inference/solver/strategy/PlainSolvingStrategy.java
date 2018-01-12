6380aeab142f5c3344a9e7c61e0a02c5ed76791f
Mier Ta <topnessman@sina.com>
2018-01-12 18:46:03 +0800

    Fix JaifBuilder bug that constructor return type is not inserted
    Print inference result in PlainSolvingStrategy

--- src/checkers/inference/solver/strategy/PlainSolvingStrategy.java
+++ src/checkers/inference/solver/strategy/PlainSolvingStrategy.java
@@ -12,6 +12,7 @@ import checkers.inference.model.Slot;
 import checkers.inference.solver.backend.Solver;
 import checkers.inference.solver.backend.SolverFactory;
 import checkers.inference.solver.frontend.Lattice;
+import checkers.inference.solver.util.PrintUtils;
 import checkers.inference.solver.util.SolverEnvironment;
 
 public class PlainSolvingStrategy extends AbstractSolvingStrategy implements SolvingStrategy {
@@ -27,7 +28,9 @@ public class PlainSolvingStrategy extends AbstractSolvingStrategy implements Sol
         Solver<?> underlyingSolver = solverFactory.createSolver(solverEnvironment, slots, constraints, lattice);
 
         Map<Integer, AnnotationMirror> result = underlyingSolver.solve();
-
+        if (result != null) {
+            PrintUtils.printResult(result);
+        }
         return new DefaultInferenceSolution(result);
     }
 
--- src/checkers/inference/util/JaifBuilder.java
+++ src/checkers/inference/util/JaifBuilder.java
@@ -240,7 +240,10 @@ public class JaifBuilder {
                     // TODO: this is not a feature but a workaround of a bug:
                     // We should create a non-empty correct ASTPath for constructor
                     if (astRecord.astPath.equals(ASTPath.empty())) {
-                        continue;
+                        if (astRecord.methodName.contains("<init>")) {
+                            astRecord = new ASTRecord(astRecord.ast, astRecord.className,
+                                    astRecord.methodName, astRecord.varName, ASTPathUtil.getMethodTypeASTPath());
+                        }
                     }
 
                     memberRecords.entries.add(new RecordValue(astRecord.astPath,annotation));
