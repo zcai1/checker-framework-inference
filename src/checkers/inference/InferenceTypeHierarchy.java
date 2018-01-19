d54fad0c769ededf7088816169d9a68f90a1cbc2
Mier Ta <topnessman@sina.com>
2018-01-19 15:51:18 +0800

    Make hack warning to info and suppress InferenceValidator error

--- src/checkers/inference/InferenceTypeHierarchy.java
+++ src/checkers/inference/InferenceTypeHierarchy.java
@@ -77,8 +77,8 @@ class InferenceEqualityComparer extends StructuralEqualityComparer {
 
         // TODO: HackMode
         if (InferenceMain.isHackMode((varAnnot1 == null || varAnnot2 == null))) {
-            InferenceMain.getInstance().logger.info(
-                "Hack:InferenceTYpeHierarchy:\n"
+            InferenceMain.getInstance().logger.warning(
+                "Hack:InferenceTYpeHierarchy:66\n"
               + "type1=" + type1 + "\n"
               + "type2=" + type2 + "\n"
             );
